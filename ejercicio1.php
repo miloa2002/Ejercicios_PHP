0ebb9d0c36fa62fb9aef0a0c5393b1da21093bb1
miloa2002 <agudelo.tapias@gmail.com>
2024-10-24 11:02:47 -0500

    Ejercicio 1 variables

---
+++ ejercicio1.php
@@ -0,0 +1,9 @@
+<?php
+/*Crea dos variables cuyo nombre sea “uno” y “dos” he imprímelas por 
+pantalla. Pon un comentario con el tipo de dato que contienen.*/
+
+$uno = "Contenido de la variable 1";
+$dos = 245;
+
+echo "Uno ".$uno." Dos". $dos;
+?>
