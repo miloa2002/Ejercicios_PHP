e34fd1961bed8e1c8fe1667d6e4505541196b8f0
miloa2002 <agudelo.tapias@gmail.com>
2024-10-24 13:14:28 -0500

    Ejercicio 8

---
+++ ejercicio8.php
@@ -0,0 +1,17 @@
+<?php
+
+/*Escribir un programa que calcule el factorial de 5.
+El factorial de un número entero N es una operación matemática que consiste en 
+multiplicar todos los factores N x (N-1) x (N-2) x ... x 1. 
+Así, el factorial de 5 (escrito como 5!) es igual a: 5! = 5 x 4 x 3 x 2 x 1 = 120*/
+
+$factorial = 1;
+$numero = 12;
+
+for($cont = 1; $cont<=$numero; $cont++) {
+    $factorial *= $cont;
+}
+
+echo "El factorial de ".$numero." es".$factorial;
+
+?>
